278dd02de70e7c35b0c75ef5c3650ea97a82a295
laegel <laegel1@gmail.com>
2023-02-16 22:37:44 +0900

    Title : querydsl test

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -84,4 +84,20 @@ public class QuerydslBasicTest {
         assertEquals(findMember.getUsername(), "member1");
 
     }
+
+    @Test
+    public void searchAndParam() throws Exception {
+        queryFactory = new JPAQueryFactory(em);
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        assertEquals(findMember.getUsername(), "member1");
+
+    }
 }
