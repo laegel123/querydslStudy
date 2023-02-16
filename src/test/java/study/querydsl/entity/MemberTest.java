2a7a715e91d305a70c33d14cda53c8dfa33d249e
laegel <laegel1@gmail.com>
2023-02-16 22:05:51 +0900

    Title : entity 등록 검증

---
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,56 @@
+package study.querydsl.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void testEntity() throws Exception {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.printf("member = " + member);
+            System.out.printf("-> member.team = " + member.getTeam());
+        }
+
+
+        // then
+
+    }
+
+}
