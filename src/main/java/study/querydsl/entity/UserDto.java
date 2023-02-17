5c11e79901ec33a8888408eebf3b65d70e1f5850
laegel <laegel1@gmail.com>
2023-02-17 16:39:16 +0900

    Title : projections test

---
+++ src/main/java/study/querydsl/entity/UserDto.java
@@ -0,0 +1,12 @@
+package study.querydsl.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserDto {
+
+    private String name;
+    private int age;
+}
