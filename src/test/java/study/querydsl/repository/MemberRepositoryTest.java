8796499fc5fb3b53cd2cf3ef0aa377f9ccbf8ad2
laegel <laegel1@gmail.com>
2023-02-20 14:18:33 +0900

    Title : spring data jpa + querydsl 추가

---
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,80 @@
+package study.querydsl.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void basicTest() throws Exception {
+        // given
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        List<Member> members = memberRepository.findAll();
+        List<Member> member1 = memberRepository.findByUsername("member1");
+
+        // when
+        assertEquals(member, findMember);
+        assertEquals(members.get(0), member);
+        assertEquals(member1.get(0), member);
+
+        // then
+
+    }
+
+    @Test
+    public void searchTest() throws Exception {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+
+        condition.setAgeGoe(35);
+        condition.setAgeGoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> members = memberRepository.search(condition);
+
+        // when
+        assertEquals(members.get(0).getUsername(), "member4");
+
+        // then
+
+    }
+
+}
